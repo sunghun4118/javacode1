58e832e311f3b995b033c23346c7d81d18164971
sunghun4118 <gn03313@naver.com>
2020-05-26 17:45:23 +0900

    26 최종

--- chap01/src/chap01/Hellotest.java
+++ chap01/src/chap01/Hellotest.java
@@ -3,6 +3,7 @@ package chap01;
 public class Hellotest {
 	public static void main(String[] args) {
 		System.out.println("pro");
+		System.out.println("탭 만큼 이동 \n 합니다.");
+		
 	}
-
 }
---
+++ chap01/src/chap01/PromotionExample.java
@@ -0,0 +1,7 @@
+package chap01;
+
+public class PromotionExample {
+	public static void main(String[] args) {
+		System.out.println(Byte.MAX_VALUE);
+	}
+}
